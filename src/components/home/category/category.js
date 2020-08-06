26b1626e83f8bbce5af3cc82e090ca3798c8d624
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-08-06 14:39:31 +1000

    Change the display of image size in Game Category

--- src/components/home/category/category.js
+++ src/components/home/category/category.js
@@ -6,19 +6,19 @@ export default function Category() {
         <div className="container game-category mt-5">
             <h3 className="d-inline-block title">Games by category: </h3>
             <div className="row my-5">
-                <div className="col-6">
+                <div className="col-12 col-md-6 col-lg-6">
                     <img src={require('../../../assets/action.jpg')}></img>
                     <NavLink to="/#" className="category-link">Action <span className="navigate px-1"><i className="fas fa-chevron-right"></i></span></NavLink>
                 </div>
-                <div className="col-6">
+                <div className="col-12 col-md-6 col-lg-6">
                     <img src={require('../../../assets/shooter.jpg')}></img>
                     <NavLink to="/#" className="category-link">Shooter <span className="navigate px-1"><i className="fas fa-chevron-right"></i></span></NavLink>
                 </div>
-                <div className="col-6">
+                <div className="col-12 col-md-6 col-lg-6">
                     <img src={require('../../../assets/rpg.jpg')}></img>
                     <NavLink to="/#" className="category-link">RPG <span className="navigate px-1"><i className="fas fa-chevron-right"></i></span></NavLink>
                 </div>
-                <div className="col-6">
+                <div className="col-12 col-md-6 col-lg-6">
                     <img src={require('../../../assets/sport.jpg')}></img>
                     <NavLink to="/#" className="category-link">Sport <span className="navigate px-1"><i className="fas fa-chevron-right"></i></span></NavLink>
                 </div>
