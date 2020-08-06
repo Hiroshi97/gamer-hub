79cdc7447eeb22035786728524fa4deceadd4c7c
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-08-06 23:21:22 +1000

    Fresh header

--- src/components/templates/header/header.jsx
+++ src/components/templates/header/header.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 export default function Header() {
     return (
         <header className="header bg-dark">
-            <div className="container header-details">
-                game-preview
-            </div>
+
         </header>
     )
 }
