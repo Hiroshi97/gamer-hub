3b3dfcb0e9016ec79adc2feb8e5b707a7547ad7f
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-08-07 01:43:08 +1000

    Implement login feature

--- src/components/authentication/login/login.jsx
+++ src/components/authentication/login/login.jsx
@@ -1,13 +1,50 @@
 import React from "react";
-import './login.scss'
+import "./login.scss";
 
 export default function Login() {
   return (
-    <div className="container-fluid login-page">
-      <div className="container login-form">
-        <div className="row justify-content-center align-content-center">
-          <div className="col">
-            <p>ABC</p>
+    <div className="container-fluid h-100 login-page">
+      <div className="container h-100 login-form">
+        <div className="h-100 row justify-content-center align-items-center">
+          <div className="col col-6">
+          <div className="text-center">
+          <img className="mx-auto" src={require('../../../assets/logo-andy.png')} alt=""/>
+          <h1>LOGIN</h1>
+          </div>
+          
+            <form>
+              <div class="form-group">
+                <label for="inputEmail">Email address</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="inputEmail"
+                />
+              </div>
+              <div class="form-group">
+                <label for="inputPassword">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="inputPassword"
+                />
+              </div>
+              <div class="form-group form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="checkRememberMe"
+                />
+                <label class="form-check-label" for="checkRememberMe">
+                  Remember Me
+                </label>
+              </div>
+              <div class="form-group form-submit text-center">
+                <button type="submit" class="btn btn-primary">
+                Login
+              </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
