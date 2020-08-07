4b3bbbdc8fc19221b246c6655222beace4887e14
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-08-08 06:17:32 +1000

    Use .env to store keys of apis

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,9 +4,14 @@ export const REQUEST = {
     baseURL: 'https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/',
     headers: {
         'Access-Control-Allow-Origin': '*',
-        'user-key': 'bcfc2527f2248a1ab7d5625a20c90223',
+        'user-key': process.env.REACT_APP_USER_KEY,
         'Accept': 'application/json',
         'Origin': 'X-Requested-With'
     },
     responseType: 'json'
 }
+
+export const API_KEY = process.env.REACT_APP_API_KEY;
+
+export const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
