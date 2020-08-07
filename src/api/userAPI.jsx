cd22457775db0a0abed7e21a1aa759286f8cc4dd
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-08-08 02:40:40 +1000

    Create signup component

---
+++ src/api/userAPI.jsx
@@ -0,0 +1,33 @@
+import {API_KEY} from '../constants/constants';
+import axios from 'axios';
+
+//SIGN UP
+export const userSignUp = async (userInfo) => {
+    const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY;
+
+    const request = {
+        email: userInfo.email,
+        password: userInfo.password,
+        returnSecureToken: true
+    }
+
+    const res = await axios.post(url, request);
+}
+
+//LOGIN
+export const userLogin = async (email, password) => {
+    const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + API_KEY;
+
+    const request = {
+        email,
+        password,
+        returnSecureToken: true
+    }
+
+    const res = await axios.post(url, request);
+
+    return res.data;
+}
+
+//SAVE USER INFO AFTER REGISTRATION
+export const saveUserInfo = () => {};
