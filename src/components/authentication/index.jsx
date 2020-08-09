9d3961775eb89eb6bd805a2cdc5bdb19ac304a5a
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-08-10 02:07:45 +1000

    Add logout component

--- src/components/authentication/index.jsx
+++ src/components/authentication/index.jsx
@@ -2,5 +2,6 @@ import Login from './login/login';
 import AuthRoute from './auth-route/authRoute';
 import Signup from './signup/signup';
 import Profile from './profile/profile';
+import Logout from './logout/logout';
 
-export { Login, Signup, AuthRoute, Profile };
+export { Login, Signup, AuthRoute, Profile, Logout };
