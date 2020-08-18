5cb2f71b1d16dd5f72a7d1e80fb6f28a11b3be1e
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-08-19 06:38:18 +1000

    Add and change some miscellaneous stuff

--- src/components/PageTitle/index.js
+++ src/components/PageTitle/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./styles.scss";
 
-export default function PageTitle({name}) {
+export default React.memo(function PageTitle({name}) {
   return (
     <h3 className="page-title font-weight-bold">
       <span id="selected-category" className="text-uppercase"></span> {name}
     </h3>
   );
-}
+})
---
+++ src/components/SubTitle/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './styles.scss';
+export default function SubTitle({title}) {
+    return (
+        <div className="sub-title">
+            <h4>{title}</h4>
+        </div>
+    )
+}
