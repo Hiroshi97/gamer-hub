6c9c0ac97e9835ee15e27b44b93a39dafb813b07
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-08-24 01:13:46 +1000

    Move pagetitle and subtitle to reusable components folder

--- src/components/Reusable/PageTitle/index.js
+++ src/components/Reusable/PageTitle/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./styles.scss";
+import PropTypes from 'prop-types';
 
-export default React.memo(function PageTitle({name}) {
+const PageTitle = ({name}) => {
   return (
     <h3 className="page-title font-weight-bold">
       <span id="selected-category" className="text-uppercase"></span> {name}
     </h3>
   );
-})
+}
+
+
+PageTitle.propTypes = {
+  name: PropTypes.string
+};
+
+export default React.memo(PageTitle)
