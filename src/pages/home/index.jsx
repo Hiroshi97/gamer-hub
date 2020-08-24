1106ed2a350d707dee59ca33c8512bd646fa4de3
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-08-25 04:44:20 +1000

    Fix a small bug

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import HomeSlider from '../../components/Home/HomeSlider';
-import HomeGames from '../../components/Home/HomeGames';;
-import HomePrelaunch from '../../components/Home/HomePrelaunch';;
+import HomeGames from '../../components/Home/HomeGames';
+import HomePrelaunch from '../../components/Home/HomePrelaunch';
 import HomeCategory from '../../components/Home/HomeCategory';
 import './home.scss';
 
