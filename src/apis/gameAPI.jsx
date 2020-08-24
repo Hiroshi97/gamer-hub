3aaa45098fcaeb817ea75dcb8b19bc788a4622cd
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-08-24 23:24:55 +1000

    Implement fetching game by ID

--- src/apis/gameAPI.jsx
+++ src/apis/gameAPI.jsx
@@ -6,8 +6,7 @@ import Axios from "axios";
 export const getGamesBasedOnPlatform = async (platform, page, limit = 0) => {
   try {
     let fetchGames = [];
-    if (platform === "0")
-        platform = "1,4919,4920";
+    if (platform === "0") platform = "1,4919,4920";
     const url = `https://cors-anywhere.herokuapp.com/https://api.thegamesdb.net/v1/Games/ByPlatformID?apikey=${GAME_API_KEY}&id=${platform}&fields=rating,genres,overview,publishers&include=boxart&page=${page}`;
 
     const response = await Axios.get(url);
@@ -36,15 +35,46 @@ export const getGamesBasedOnPlatform = async (platform, page, limit = 0) => {
 
       return { ...game, img: cover_img };
     });
-    
-    //Limit the length of games
-    if (limit)
-        return fetchGames.slice(0, limit);
-    return fetchGames;
 
+    //Limit the length of games
+    if (limit) return fetchGames.slice(0, limit);
+    return fetchGames;
   } catch (err) {
     console.log(err);
   }
+};
+
+export const getGamesById = async (id) => {
+  let fetchGame = [];
+  const url = `https://cors-anywhere.herokuapp.com/https://api.thegamesdb.net/v1/Games/ByGameID?apikey=${GAME_API_KEY}&id=${id}&fields=rating,genres,overview,publishers&include=boxart`;
+
+  const response = await Axios.get(url);
+  fetchGame = response.data.data.games;
+
+  //Get the boxart
+  let covers = Object.values(response.data.include.boxart.data);
+
+  //Get the name of each genre
+  fetchGame = fetchGame.map((game, index) => {
+    if (game.genres && game.genres.length > 0)
+      //Check whether a game has its genres
+      game.genres = game.genres.map((genre) => GAME_GENRES[genre].name);
+    else game.genres = [GAME_GENRES[0].name];
+
+    let cover_img = "";
+
+    //Generate an url of cover img
+    covers[index].some((cover) => {
+      if (cover.side === "front") {
+        cover_img = "https://cdn.thegamesdb.net/images/large/" + cover.filename;
+        return true;
+      }
+    });
+
+    return { ...game, img: cover_img };
+  });
+
+  return fetchGame;
 };
 
 //GET COMING SOON GAME
