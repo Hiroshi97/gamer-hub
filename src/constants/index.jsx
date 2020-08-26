f0a8d3e96c29fe6f5329a995be4b3e6b85c549d9
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-08-26 23:41:28 +1000

    Add database name to constant and rename file to index

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -16,12 +16,12 @@ export const API_KEY = process.env.REACT_APP_FIREBASE_API_KEY;
 
 export const GAME_API_KEY = process.env.REACT_APP_THEGAMESDB_API_KEY;
 
+export const DATABASE_NAME = process.env.REACT_APP_DATABASE_NAME;
 
 //REGEX VALIDATION
 export const EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 export const NAME_REGEX = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
-
 
 // GAME GENRES
 export const GAME_GENRES = [
