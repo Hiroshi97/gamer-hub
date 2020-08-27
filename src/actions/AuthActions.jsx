a0ab4731bf5bc3d6cf1be63abe83ab08e2c61999
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-08-27 22:05:40 +1000

    Add signup actions

--- src/actions/AuthActions.jsx
+++ src/actions/AuthActions.jsx
@@ -11,6 +11,19 @@ export const LoginFailure = () => ({
   type: 'LOGIN_FAILURE'
 })
 
+export const SignupRequest = () => ({
+  type: "SIGNUP_REQUEST",
+});
+
+export const SignupSuccess = (userInfo) => ({
+  type: 'SIGNUP_SUCCESS',
+  payload: userInfo
+})
+
+export const SignupFailure = () => ({
+  type: 'SIGNUP_FAILURE'
+})
+
 export const LogoutSuccess = () => ({
   type: 'LOGOUT_SUCCESS',
 })
--- src/reducers/AuthReducers.jsx
+++ src/reducers/AuthReducers.jsx
@@ -7,31 +7,34 @@ const initialState = {
 const AuthReducer = (state = initialState, action) => {
   switch (action.type) {
     case "LOGIN_REQUEST":
+    case "SIGNUP_REQUEST":
       return {
         ...state,
         result: false,
-        loading: true
+        loading: true,
       };
 
     case "LOGIN_SUCCESS":
+    case "SIGNUP_SUCCESS":
       return {
         userInfo: action.payload,
         result: true,
-        loading: false
+        loading: false,
       };
 
     case "LOGIN_FAILURE":
+    case "SIGNUP_FAILURE":
       return {
         ...state,
         result: false,
-        loading: false
+        loading: false,
       };
 
     case "LOGOUT_SUCCESS":
       return {
         userInfo: {},
         result: false,
-        loading: false
+        loading: false,
       };
 
     default:
