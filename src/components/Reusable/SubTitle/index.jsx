58c07ec6abb748b0219b4a1135de6352e6e15e40
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-08-29 06:05:44 +1000

    Redesign subtitle

--- src/components/Reusable/SubTitle/index.jsx
+++ src/components/Reusable/SubTitle/index.jsx
@@ -3,9 +3,14 @@ import PropTypes from "prop-types";
 import './styles.scss';
 
 export default function SubTitle({title}) {
+    let fixedTitle = '';
+    if (title.split(" ").length === 2) {
+        fixedTitle = title.split(" ");
+    } else fixedTitle = [title];
+
     return (
         <div className="sub-title">
-            <h4>{title}</h4>
+            {fixedTitle.length > 1 ? <h4><span className="text-danger">{fixedTitle[0] + " "}</span>{fixedTitle[1]}</h4> : <h4>{fixedTitle[0]}</h4>} 
         </div>
     )
 }
