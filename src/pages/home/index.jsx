ad238d8d62cb4d78e79ff4e0077d5e4720b90221
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-08-31 17:23:37 +1000

    scroll to top whenever return to homepage from somewhere

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,8 +4,12 @@ import HomeGames from '../../components/Home/HomeGames';
 import HomePrelaunch from '../../components/Home/HomePrelaunch';
 import HomeCategory from '../../components/Home/HomeCategory';
 import './home.scss';
+import { useEffect } from 'react';
 
 export default function Home() {
+    useEffect(() => {
+        window.scrollTo(0,0);
+    }, [])
     return (
         <div className="home-content">
             <HomeSlider/>
